2fe60ac662f0de2bdb2dbb9cdbee9d6d2259b05f
Z4que <114612941+Zaque-69@users.noreply.github.com>
2025-06-12 23:13:00 +0300

    Add files via upload

--- shell.nix
+++ shell.nix
@@ -2,9 +2,8 @@
 
 pkgs.mkShell{
   nativeBuildInputs = with pkgs; [
-    python3
-    python311Packages.pillow
-    python311Packages.opencv4
+    python313Packages.pillow
+    python313Packages.opencv4
   ];
 
   shellHook = ''
